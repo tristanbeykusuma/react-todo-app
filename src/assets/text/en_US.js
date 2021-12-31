280921bb9be383820dc091c5b4a4243e2e167f9d
Tristan Kusuma <91164364+tristanbeykusuma@users.noreply.github.com>
2021-12-31 09:13:13 +0800

    Add files via upload

---
+++ src/assets/text/en_US.js
@@ -0,0 +1,3 @@
+export const MSG_NO_ITEMS = 'Tidak ada tugas.';
+export const INFO_SHORTCUT_KEYS = 'Klik `/` untuk cari tugas dan `N` untuk menambah tugas baru.';
+export const INFO_CANCEL_SHORTCUT_KEY = 'Klik `Esc` untuk keluar.';
