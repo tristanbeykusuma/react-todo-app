57eda71d7a6702a3383bb68ab1041b6083d5c1b4
Tristan Kusuma <91164364+tristanbeykusuma@users.noreply.github.com>
2022-01-01 12:30:07 +0800

    Update Header.js

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,7 @@ export default function Header(props) {
     return (
         <header>
             <h1>To Do List</h1>
+            <h5>-----------------------------------by Tristan Bey Kusuma (2008561053)----------------------------------</h5>
             <InputWrapper {...props}/>
         </header>
     );
